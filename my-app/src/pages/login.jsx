91e5a1bb5fe6ab7681d184a818376e5d6a589887
Natalie Kyresa Feliciano <nkfelic1@email.asu.edu>
2026-03-28 17:04:45 -0700

    Added checks for authorization and protected routing

--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
