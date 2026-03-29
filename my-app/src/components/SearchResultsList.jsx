d8572693befdc1da4f579ceed4acd44d3721113a
Natalie Kyresa Feliciano <nkfelic1@email.asu.edu>
2026-03-29 00:29:04 -0700

    Map search results to client profile page

--- my-app/src/components/SearchResultsList.jsx
+++ my-app/src/components/SearchResultsList.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from 'react-router-dom';
+
 function SearchResultsList({ results = [] }) {
+  const navigate = useNavigate();
+
   if (!results.length) {
     return <div className="search-result-empty">No clients found.</div>;
   }
@@ -6,7 +10,10 @@ function SearchResultsList({ results = [] }) {
   return (
     <div>
       {results.map((client, index) => (
-        <div className="search-result-item" key={`${client.first_name}-${client.last_name}-${index}`}>
+        <div className="search-result-item" key={client.client_id ?? `${client.first_name}-${client.last_name}-${index}`}
+             onClick={() => navigate(`/client/view/${client.client_id}`)}
+             style={{ cursor: 'pointer' }}
+        >
           {client.first_name} {client.last_name}
         </div>
       ))}
@@ -15,4 +22,3 @@ function SearchResultsList({ results = [] }) {
 }
 
 export default SearchResultsList
-
